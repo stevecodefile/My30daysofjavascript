0f20e7679e7dd55700428333c24e1eaada8fae2e
Steve Noel <115962692+stevecodefile@users.noreply.github.com>
2023-07-21 11:45:55 +0530

    Add files via upload

---
+++ #day02/datatype.js
@@ -0,0 +1,12 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge ='30 Days Of JavaScript'
+//Print the string on the browser console using console.log()
+console.log(challenge)
+
+//Print the length of the string on the browser console using console.log()
+console.log(challenge.length)
+
+//Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase())
+//Change all the string characters to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase())
